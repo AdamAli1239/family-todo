6467b2fcbd744307e0611836097b0821f05f6484
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-06-25 11:47:57 -0400

    Added the functionality for a user to be able to delete a chore

---
+++ src/components/userCreate.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <form @click.prevent>
+      <label>Role</label>
+      <input type="text" required v-model="userRole" />
+      <label>Name</label>
+      <input type="text" required v-model="userName" />
+      <label>Chores</label>
+      <input type="text" required v-model="choreList" />
+
+      <input type="submit" value="Submit" @click="createUSER" />
+    </form>
+
+    <div v-if="showDIV">
+      <h1>Name:{{ userName }}</h1>
+
+      <h2>Role: {{ userRole }}</h2>
+
+      <h3>Chores:</h3>
+
+      <div v-for="chore in choreList" :key="chore" :class="chore">
+        <p>{{ chore }}</p>
+
+        <span><button @click="updateBtn">Update</button></span>
+        <span><button @click="deletebtn(chore)">Delete</button></span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: "",
+      userRole: "",
+
+      showDIV: false,
+      choreList: [],
+    };
+  },
+  methods: {
+    createUSER() {
+      this.showDIV = true;
+    },
+    updateBtn() {},
+    deletebtn(chore) {
+      console.log(chore);
+
+      const removeChore = document.querySelector(`.${chore}`);
+
+      removeChore.remove();
+    },
+  },
+};
+</script>
+
+<style></style>
