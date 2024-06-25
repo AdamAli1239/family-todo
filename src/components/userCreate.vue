177174d707b98a142a5afadc090761587f30e92e
AdamAli1239 <154936108+AdamAli72@users.noreply.github.com>
2024-06-25 13:37:13 -0400

    Now the user can update, delete chores without the button/text input satying on teh screen

--- src/components/userCreate.vue
+++ src/components/userCreate.vue
@@ -2,9 +2,9 @@
   <div>
     <form @click.prevent>
       <label>Role</label>
-      <input type="text" required v-model="userRole" />
+      <input type="text" required v-model="userRole" class="role" />
       <label>Name</label>
-      <input type="text" required v-model="userName" />
+      <input type="text" required v-model="userName" class="name" />
       <label>Chores</label>
       <input type="text" required v-model="choreList" />
 
@@ -21,7 +21,7 @@
       <div v-for="chore in choreList" :key="chore" :class="chore">
         <p>{{ chore }}</p>
 
-        <span><button @click="updateBtn">Update</button></span>
+        <span><button @click="updateBtn(chore)">Update</button></span>
         <span><button @click="deletebtn(chore)">Delete</button></span>
       </div>
     </div>
@@ -34,7 +34,7 @@ export default {
     return {
       userName: "",
       userRole: "",
-
+      textBox: 0,
       showDIV: false,
       choreList: [],
     };
@@ -42,8 +42,40 @@ export default {
   methods: {
     createUSER() {
       this.showDIV = true;
+      const ROLE = document.querySelector(".role");
+      ROLE.value = "";
+
+      const NAME = document.querySelector(".name");
+      NAME.value = "";
     },
-    updateBtn() {},
+
+    updateBtn(chore) {
+      this.textBox = 1;
+      console.log("HERE");
+      console.log(this.textBox);
+      if (this.textBox == 1) {
+        this.textBox = 0;
+        const userText = document.createElement("input");
+        const submitText = document.createElement("button");
+        submitText.textContent = "Click me to update the chore";
+        const updateChore = document.querySelector(`.${chore}`);
+        updateChore.appendChild(userText);
+        updateChore.append(submitText);
+        console.log(updateChore);
+        const text = updateChore.querySelector("p");
+        console.log(text.textContent);
+        submitText.addEventListener("click", (e) => {
+          console.log("IN HERE");
+          console.log(userText.value);
+          text.textContent = userText.value;
+          userText.remove();
+          submitText.remove();
+        });
+      }
+
+      // text.textContent =
+      //  updateChore.textContent= userText
+    },
     deletebtn(chore) {
       console.log(chore);
 
